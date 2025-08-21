b78f3c34dc7183e75e6d858c61c4cad94ff3ad23
liu si <2218893215@qq.com>
2025-08-21 14:15:48 +0000

    终于修好了呜呜

--- dockertest1/container/container_process.go
+++ dockertest1/container/container_process.go
@@ -1,1 +1,30 @@
 package container
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
+	// 创建匿名管道用于传递参数，将readPipe作为子进程的ExtraFiles，子进程从readPipe中读取参数
+	// 父进程中则通过writePipe将参数写入管道
+	readPipe, writePipe, err := os.Pipe()
+	if err != nil {
+		log.Errorf("New pipe error %v", err)
+		return nil, nil
+	}
+	cmd := exec.Command("/proc/self/exe", "init")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
+	}
+	if tty {
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+	cmd.ExtraFiles = []*os.File{readPipe}
+	//指定 cmd 的工作目录为我们前面准备好的用于存放 busybox rootfs的目录，暂时固定为 /root/busybox
+	cmd.Dir = "/root/busybox"
+	return cmd, writePipe
+}
--- dockertest1/go.mod
+++ dockertest1/go.mod
@@ -9,6 +9,7 @@ require (
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/sys v0.33.0 // indirect
